d4db7a87604ce62a1c00e1c4e2c0cab4af974928
Félix Noiseux <rap.felix@hotmail.com>
2018-11-13 12:22:32 -0500

    Test

--- HackfestFinal/HackfestFinal/Startup.cs
+++ HackfestFinal/HackfestFinal/Startup.cs
@@ -27,7 +27,7 @@ namespace HackfestFinal
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Loik est cool");
             });
         }
     }
