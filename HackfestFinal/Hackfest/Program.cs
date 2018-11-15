527b8c7c651f6a721cce60652bcec5785c72d18f
loik champagne <loik1010@live.com>
2018-11-14 18:08:08 -0700

    restart a 0

--- HackfestFinal/Hackfest/Program.cs
+++ HackfestFinal/Hackfest/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Hackfest
+namespace HackFest
 {
     public class Program
     {
