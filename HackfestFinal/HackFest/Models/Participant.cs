8039f0f010b9bdb7cd33335eb1a3ff81ad3b2aad
loik champagne <loik1010@live.com>
2018-11-14 18:40:13 -0700

    Création de la classe participant

---
+++ HackfestFinal/HackFest/Models/Participant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackFest.Models
+{
+    public class Participant
+    {
+        public int ID_Participant { get; set; }
+
+        [Required(ErrorMessage = "SVP, inscrivez votre prénom")]
+        public string Prenom { get; set; }
+
+        [Required(ErrorMessage = "SVP inscrivez votre nom")]
+        public string Nom { get; set; }
+
+        [Required(ErrorMessage = "SVP inscrivez votre couriel")]
+        public string Couriel { get; set; }
+
+        [Required(ErrorMessage = "SVP spécifier une affiliaton")]
+        public string Affiliation { get; set; }
+
+        [Required(ErrorMessage = "SVP veuillez spécifier une date")]
+        public DateTime Date_inscription { get; set; }
+
+        [Required(ErrorMessage = "SVP inscrivez un montant")]
+        public int Montant { get; set; }
+    }
+}
