097e202539a98452a72ef820d39f58b946352f4c
loik champagne <loik1010@live.com>
2018-11-14 19:09:43 -0700

    on peut se connecter a internet

---
+++ HackfestFinal/HackFest/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackFest.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
