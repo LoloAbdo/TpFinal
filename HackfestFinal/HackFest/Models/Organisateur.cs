91b005fd0bc4a4f94faa1142d99bd827b6034fbb
Félix Noiseux <rap.felix@hotmail.com>
2018-12-04 12:24:10 -0500

    Voici voila

--- HackfestFinal/HackFest/Models/Organisateur.cs
+++ HackfestFinal/HackFest/Models/Organisateur.cs
@@ -17,17 +17,12 @@ namespace HackFest.Models
             ResponsableDesArticles
         }
 
-
         [Key]
         public int ID_Organisateur { get; set; }
         
-        [ForeignKey("Participant"),]
+        [ForeignKey("Participant")]
         public int ID_Participant { get; set; }
 
-        public Participant Participant { get; set; }
-
-        //[RegularExpression(@"^ (?=.*[a - z])(?=.*[A - Z])(?=.*\d).{8, 15}$)",
-        //                  ErrorMessage = "Il vous faut au moins 8 caracteres, une majuscule et un chiffre")]
         [StringLength(20, MinimumLength = 8)]
         public string MotDePasse { get; set; }
 
